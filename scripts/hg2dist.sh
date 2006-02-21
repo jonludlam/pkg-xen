7f8790fe43ecbd3725e05508a6fda4e7659e4e5b
ultrotter <ultrotter@19d14552-db0c-0410-aa19-817c7d738a49>
2006-02-21 09:24:17 +0000

    Add a couple more checks to hg2dist and create orig tar too
    git-svn-id: svn://anonscm.debian.org/svn/pkg-xen/trunk@46 19d14552-db0c-0410-aa19-817c7d738a49

--- scripts/hg2dist.sh
+++ scripts/hg2dist.sh
@@ -18,12 +18,27 @@ CHANGESET=$( (cd $HGDIR; $HG log | head -1 ) | sed -e 's/ //g;' | cut -d: -f2)
 RELEASE=$( (cd $HGDIR; $HG log | grep "tag:.*RELEASE" | head -1) | sed -e 's/ //g; s/tag:RELEASE-//')
 
 DESTDIR="xen-${RELEASE}+hg${CHANGESET}"
+DESTTAR="xen_${RELEASE}+hg${CHANGESET}.orig.tar.gz"
+
+if [ -d $DESTDIR ]; then
+	echo "Destination directory $DESTDIR already exists"
+	exit 1
+fi
+
+if [ -f $DESTTAR ]; then
+	echo "Destination file $DESTTAR already exists"
+fi
 
 echo "Exporting xen to $DESTDIR..."
 
-mkdir $DESTDIR
+mkdir $DESTDIR || exit 1;
+
 cp -R $HGDIR/* $DESTDIR/ # This skips everything starting with a ., which is what we want
 (cd $HGDIR; $HG log) > $DESTDIR/ChangeLog
+
+echo "Creating orig file $DESTTAR..."
+
+tar -zc $DESTDIR -f $DESTTAR || exit 1;
 
 echo "DONE"
 echo "Now please svn export your debian directory in $DESTDIR and check that the changelog version matches"
