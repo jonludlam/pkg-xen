a81bb844868cd615555929496e1d8dfb7af8d4ff
waldi <waldi@19d14552-db0c-0410-aa19-817c7d738a49>
2007-11-18 19:20:26 +0000

    debian/lib/python/debian_xen/__init__.py: Update message.
    git-svn-id: svn://anonscm.debian.org/svn/pkg-xen/trunk@471 19d14552-db0c-0410-aa19-817c7d738a49

--- xen-3/debian/lib/python/debian_xen/__init__.py
+++ xen-3/debian/lib/python/debian_xen/__init__.py
@@ -13,7 +13,7 @@ def _setup():
     global support
     support = '/usr/src/linux-support-%s' % version
     if not os.path.exists(support):
-        raise RuntimeError("Can't find %s" % support)
+        raise RuntimeError("Can't find %s, please install the linux-support-%s package" % (support, version))
     sys.path.append('%s/lib/python' % support)
 
 _setup()
