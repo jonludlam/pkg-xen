85ea10e053f4dbb8e8e7eb42541aeff4b118ea80
waldi <waldi@19d14552-db0c-0410-aa19-817c7d738a49>
2011-04-10 09:26:14 +0000

    Remove old scripts and patches.
    git-svn-id: svn://anonscm.debian.org/svn/pkg-xen/trunk@862 19d14552-db0c-0410-aa19-817c7d738a49

--- scripts/create-kernel-patch.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-LINUX_VERSION=2.6.12
-
-make linux-2.6-xen-config CONFIGMODE=oldconfig
-cat linux-$LINUX_VERSION-xen/Makefile | grep -v "EXTRAVERSION =" | sed '/SUBLEVEL\ =/a EXTRAVERSION\ =' > linux-$LINUX_VERSION-xen/Makefile
-diff -Nurp pristine-linux-$LINUX_VERSION/ linux-$LINUX_VERSION-xen/ >linux-$LINUX_VERSION-xen.patch
